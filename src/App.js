d3fccda0d30b8aea1bbd5cf0cf3758040a5898f9
“tarek994” <ghali.tarek.123@hotmail.com>
2022-09-02 05:06:08 +0200

    fdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React,{Component} from "react";
+import React,{useState} from "react";
+import axios from "axios";
 
 
 function App() {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=dallas&units=metric&appid=6b42ac7df85a4e3c6a182a15d6784e03';
+
+
+
+
   return (
-    <div className="App">
-      <h1> test</h1>
+    <div className="app">
+        <div className="container">
+            <div className="top">
+              <div className="location">
+                <p>Dallas</p>
+              </div>
+              <div className="temp">
+                <h1>60 F</h1>
+              </div>
+              <div className="description">
+                <p>Clouds</p>
+              </div>
+            </div>
+              <div className="bottom">
+                <div className="feels">
+                <p>65 F</p>
+                </div>
+                <div className="humidity">
+                  <p>20%</p>
+                </div>
+                <div className="wind">
+                  12 MPH
+                </div>
+              </div>
+        </div>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from "./reportWebVitals 04-04-16-100"
+import reportWebVitals from "./reportWebVitals 04-04-16-100";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
