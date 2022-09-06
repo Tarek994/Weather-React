de6e242537f7ea8d76028831742ea3bbf97a22e4
“tarek994” <ghali.tarek.123@hotmail.com>
2022-09-07 01:07:01 +0200

    sdfs34e

--- src/App.js
+++ src/App.js
@@ -33,29 +33,36 @@ function App() {
         <div className="container">
             <div className="top">
               <div className="location">
-                <p>Dallas</p>
+                <p>{data.name}</p>
               </div>
               <div className="temp">
-                <h1>60 F</h1>
+                {data.main ? <h1>{data.main.temp }˚F</h1> : null}
               </div>
               <div className="description">
-                <p>Clouds</p>
+                {data.weather ? <p>{data.weather[0].main}</p> : null}
               </div>
             </div>
-              <div className="bottom">
-                <div className="feels">
-                <p className="bold">65 F</p>
-                <p>Feels Like</p>
-                </div>
-                <div className="humidity">
-                  <p className="bold">20%</p>
-                  <p>Humidity</p>
-                </div>
-                <div className="wind">
-                  <p className="bold">12 MPH</p>
-                  <p>Wind Speed</p>
-                </div>
-              </div>
+
+            {data.name != undefined && 
+            <div className="bottom">
+            <div className="feels">
+              {data.main ? <p>{data.main.feels_like}˚F</p> : null}
+            <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+
+            }
+              
+
+
         </div>
       
     </div>
