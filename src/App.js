d8b345c67364949945d44e185b7e672fac723a20
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-26 23:41:33 +0200

    Fix(app-api): fix protocol by changing it to HTTPS

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import axios from "axios";
 
 function App() {
     const [data,setData] = useState({});
-    const [location,setLocation] = useState('')
+    const [location,setLocation] = useState('');
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&units=metric&appid=6b42ac7df85a4e3c6a182a15d6784e03`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&units=metric&appid=6b42ac7df85a4e3c6a182a15d6784e03`;
     const searchLocation = (event) => {
       if( event.key === 'Enter'){
         axios.get(url).then((response) => {
