178dd1ef91ed7d16ea75dae6d865e7cbeb62384e
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-28 23:23:07 +0200

    import: Services component to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Hero from "./sections/Hero";
+import Services from "./sections/Services";
 
 function App() {
 
     return (
-      <>
+      <div className="">
 
       <Hero/>
-
-      </>
+      <Services/>
+      </div>
     )
 
 
