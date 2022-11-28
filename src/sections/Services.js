d311bed7a8072cf673d3b798ab83cad9e03ccf24
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-28 23:22:05 +0200

    new(Services): create a new Services section

---
+++ src/sections/Services.js
@@ -0,0 +1,12 @@
+
+const Services = () => {
+  return (
+    <div className="services">
+       <div className="service-title">
+        <h1>Services</h1>
+       </div>
+    </div>
+  )
+}
+
+export default Services
